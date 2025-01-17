92c6ce52d0bda1b259d47fefb2fe88639dfa8ad1
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2025-01-17 14:22:03 -0300

    jbdc - deletarDados

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import db.DB;
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        // programa para deletar dados no banco de dados
+
+        Connection conn = null;
+        PreparedStatement st = null; // classe para criar os comandos SQL
+
+
+    }
+}
